82ad69ab8f01b2a679f21aac029bbdcb3f5dd8d8
MinhQuan-21130491 <21130491@st.hcmuaf.edu.vn>
2023-11-13 23:02:53 +0700

    lab6

---
+++ AI_Lab6/src/task1/HillClimbing.java
@@ -0,0 +1,27 @@
+package task1;
+
+import java.util.Arrays;
+
+public class HillClimbing {
+	public Node execute(Node initialState) {
+		Node current = initialState;
+		Node neightBor = null;
+		while (true) {
+			neightBor = current.getBestCandidate();
+			if (current.getH() > neightBor.getH()) {
+				current = neightBor;
+			} else {
+				return current;
+			}
+		}
+	}
+
+	public Node executeHillClimbingWithRandomRestart(Node initialState) {
+		// Enter your code here.
+		return null;
+	}
+
+	public static void main(String[] args) {
+		new HillClimbing().execute(new Node()).displayBoard();
+	}
+}
---
+++ AI_Lab6/src/task1/Queen.java
@@ -0,0 +1,49 @@
+package task1;
+
+public class Queen {
+	private int row;
+	private int column;
+
+	public Queen(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+
+	public void move() {
+		setRow(row++);
+		if(row >= Node.N) {
+			setRow(0);
+		}
+	}
+	// check whether this Queen can attack the given Queen (q)
+	public boolean isConflict(Queen q) {
+		if(this.row == q.getRow()) return true;
+		if(calRow(row, q.getRow()) == calCol(column,q.getColumn())) return true;
+		return false;
+	}
+	public int calRow(int r1, int r2) {
+		return Math.abs(r1-r2);
+	}
+	public int calCol(int c1, int c2) {
+		return Math.abs(c1-c2);
+	}
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "(" + row + ", " + column + ")";
+	}
+}
